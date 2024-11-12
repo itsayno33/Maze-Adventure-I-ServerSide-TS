f613d2d71d42b89d34a8796a07dc40d70cde1326
itsayno33 <itsayno33@gmail.com>
2024-11-12 18:33:31 +0900

    Bug Fixed.

--- src/routes/maiGuld.ts
+++ src/routes/maiGuld.ts
@@ -5,17 +5,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.type('.txt');
   res.send('respond with a maiGuld');
 });
 
 router.post('/newGame', function(req: express.Request, res: express.Response, next: express.NextFunction) {
   const ret = newGame(req.body);
+  res.type('application/json');
   res.json(ret);
+});
+router.get('/newGame', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.type('.txt');
+  res.send('respond with a maiNewGame');
 });
 
 router.post('/newHres', function(req: express.Request, res: express.Response, next: express.NextFunction) {
   const ret = newHres(req.body);
+  res.type('application/json');
   res.json(ret);
+});
+
+router.get('/newHres', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.type('.txt');
+  res.send('respond with a maiNewHres');
 });
 
 module.exports = router;
