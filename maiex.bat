bc6e31672e2afa18a42bd0fcb8f1e12bfb0ca916
itsayno33 <itsayno33@gmail.com>
2024-12-02 16:44:36 +0900

    パソコン変更に伴うフォルダ名変更

--- maiex.bat
+++ maiex.bat
@@ -1,2 +1,2 @@
-robocopy D:\Top\Dev\ts\maiex\src\views  D:\Top\Dev\Docker\Mai\bck\app\views  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
-robocopy D:\Top\Dev\ts\maiex\src\public D:\Top\Dev\Docker\Mai\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
+robocopy E:\Dev\vscode\mai\maiex\src\views  E:\Dev\vscode\mai\Mai_docker\bck\app\views  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
+robocopy E:\Dev\vscode\mai\maiex\src\public E:\Dev\vscode\mai\Mai_docker\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
