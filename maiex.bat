055bc92ad404a379ad914aea9a5049192aba359f
itsayno33 <itsayno33@gmail.com>
2024-12-07 11:35:07 +0900

    .

--- maiex.bat
+++ maiex.bat
@@ -1,2 +1,2 @@
-robocopy E:\Dev\vscode\mai\maiex\src\views  E:\Dev\vscode\mai\Mai_docker\bck\app\views  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
-robocopy E:\Dev\vscode\mai\maiex\src\public E:\Dev\vscode\mai\Mai_docker\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
+robocopy D:\Dev\ts\maiex\src\views  D:\Docker\Mai_Docker\bck\app\views  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
+robocopy D:\Dev\ts\maiex\src\public D:\Docker\Mai_Docker\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
