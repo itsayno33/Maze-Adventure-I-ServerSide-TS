571ce4472d157024f597dbff1ac6fedad520d4c5
itsayno33 <itsayno33@gmail.com>
2025-06-23 19:20:43 +0900

    ディレクトリ構成変更に伴うファイル変更６

--- maiex.bat
+++ maiex.bat
@@ -1,2 +1,2 @@
 robocopy C:\__Dev\mai\maiex\src\views  C:\__Dev\mai\docker\bck\app\views  /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
-robocopy C:\__Dev\mai\maiex\src\public C:\__Dev\docker\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
+robocopy C:\__Dev\mai\maiex\src\public C:\__Dev\mai\docker\bck\app\public /MIR /EFSRAW /NP /V /R:1 /W:1 /IT /LOG+:maiex.log
